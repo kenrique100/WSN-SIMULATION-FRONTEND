81353ae0bf53e964cb8c6492816cf0ecc5d895b3
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2025-05-13 17:17:47 -0400

    Update ProtectedRoute.tsx

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,7 +1,7 @@
-// src/routes/ProtectedRoute.tsx
+/*
 import { Navigate, useLocation } from 'react-router-dom';
 import Loading from '@/components/common/Loading';
-import { useAuth } from '@/context/AuthContext';
+import { useAuth } from '@/contexts/AuthContext';
 import { ReactNode, useEffect, useState } from 'react';
 
 interface ProtectedRouteProps {
@@ -10,31 +10,31 @@ interface ProtectedRouteProps {
 }
 
 export default function ProtectedRoute({ children, roles }: ProtectedRouteProps) {
-    /*    const { user, isLoading, error } = useAuth();
-        const location = useLocation();
-        const [initialLoad, setInitialLoad] = useState(true);
+    const { user, isLoading, error } = useAuth();
+    const location = useLocation();
+    const [initialLoad, setInitialLoad] = useState(true);
 
-        useEffect(() => {
-            if (!isLoading) {
-                setInitialLoad(false);
-            }
-        }, [isLoading]);
-
-        if (initialLoad) {
-            return <Loading fullScreen />;
+    useEffect(() => {
+        if (!isLoading) {
+            setInitialLoad(false);
         }
+    }, [isLoading]);
 
-        if (error) {
-            return <Navigate to="/login" state={{ from: location, error }} replace />;
-        }
+    if (initialLoad) {
+        return <Loading fullScreen />;
+    }
 
-        if (!user) {
-            return <Navigate to="/login" state={{ from: location }} replace />;
-        }
+    if (error) {
+        return <Navigate to="/login" state={{ from: location, error }} replace />;
+    }
 
-        if (roles && !roles.includes(user.role)) {
-            return <Navigate to="/" replace />;
-        }*/
+    if (!user) {
+        return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    if (roles && !roles.includes(user.role)) {
+        return <Navigate to="/" replace />;
+    }
 
     return <>{children}</>;
-}
+}*/
