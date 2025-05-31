a7aa7125ab16d90ed589f4d67717a8840933e7b8
Edwin  Ngwa <ngwakenri2016@gmail.com>
2025-05-31 16:33:14 -0400

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,29 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    // Load env file based on `mode` in the current working directory
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [react()],
-        define: {
-            global: {},
-        },
-        server: {
-            port: 3000,
-            proxy: {
-                '/api': {
-                    target: env.VITE_API_URL,
-                    changeOrigin: true,
-                    secure: false,
-                },
-                '/ws': {
-                    target: env.VITE_API_URL,
-                    ws: true,
-                    changeOrigin: true,
-                },
+export default defineConfig({
+    plugins: [react()],
+    define: {
+        global: {},
+    },
+    server: {
+        port: 3000,
+        proxy: {
+            '/api': {
+                target: 'https://wsn-monitoring-1-0-0-1.onrender.com',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/ws': {
+                target: 'https://wsn-monitoring-1-0-0-1.onrender.com',
+                ws: true,
+                changeOrigin: true,
             },
         },
-        resolve: {
-            alias: {
-                '@': '/src',
-            },
+    },
+    resolve: {
+        alias: {
+            '@': '/src',
         },
-    };
+    },
 });
