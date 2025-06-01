f21f4daa3210a8a53175c6d92a5ae827cab5a271
Edwin  Ngwa <ngwakenri2016@gmail.com>
2025-06-01 14:51:58 -0400

    updated

--- src/types/notification.ts
+++
@@ -1,9 +0,0 @@
-export interface Notification {
-  id: string;
-  title: string;
-  message: string;
-  read: boolean;
-  createdAt: string;
-  type: string;
-  metadata?: Record<string, unknown>;
-}
